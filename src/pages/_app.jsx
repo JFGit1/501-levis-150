438bfb869dbf7e77baeff537cb6052e5e4c2df4e
JF Git <juliofarias+git1@gmail.com>
2023-07-24 14:08:08 -0300

    Navigation refactoring - 0.5

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps, router }) {
 		<>
 			<TransitionProvider>
 				<Transition />
-				<Header />
+				{/* <Header /> */}
 				<AnimatePresence
 					mode='wait'
 					initial={false}
